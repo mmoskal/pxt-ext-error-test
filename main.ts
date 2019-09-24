3d896ffbf3184abc52f1ecd579ebd4d397f58e74
Franklin Tse <FranklinWhale@users.noreply.github.com>
2019-09-24 15:44:04 +0800

    For test

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% block="JSON" color=#FF0000
+namespace Test {
+    //% block="Run %fnName on %data if it exists"
+    export function runIfExists(data: any, fnName: string) {
+        if (typeof data === "object" && typeof data[fnName] === "function") {
+            data[fnName]();
+        }
+    }
+}
